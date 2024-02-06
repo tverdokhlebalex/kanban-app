942ae7dcebd87ac3aacd24a49b9cc33641989516
Alexander <tverdokhlebalex@me.com>
2024-02-06 16:22:59 +0400

    2

--- kanban-board/src/components/App/App.jsx
+++ kanban-board/src/components/App/App.jsx
@@ -1,37 +1,39 @@
-import Header from '../Header/Header';
-import Main from '../Main/Main';
-import Footer from '../Footer/Footer';
-import styles from './App.module.scss'
-
 import React, { useState, useEffect, createContext } from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from '../Main/Main';
+import TaskDetails from '../TaskDetails/TaskDetales'; 
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import styles from './App.module.scss'; 
 
-export const KanbanContext = createContext(null); 
+export const KanbanContext = createContext(null);
 
 const App = () => {
-   
-    const [kanbanData, setKanbanData] = useState({});
-  
-    useEffect(() => {
-      const loadedData = JSON.parse(localStorage.getItem('kanbanData')) || {};
-      setKanbanData(loadedData);
-    }, []);
-  
-    useEffect(() => {
-      localStorage.setItem('kanbanData', JSON.stringify(kanbanData));
-    }, [kanbanData]);
-  
-    return (
-      <Router>
-        <div className={styles.app}>
-          <Header />
-          <KanbanContext.Provider value={{ kanbanData, setKanbanData }}>
-            <Main />
-          </KanbanContext.Provider>
-          <Footer />
-        </div>
-      </Router>
-    );
-  };
-  
-  export default App;
+  const [kanbanData, setKanbanData] = useState({});
+
+  useEffect(() => {
+    const loadedData = JSON.parse(localStorage.getItem('kanbanData')) || {};
+    setKanbanData(loadedData);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('kanbanData', JSON.stringify(kanbanData));
+  }, [kanbanData]);
+
+  return (
+    <Router>
+      <div className={styles.app}>
+        <Header />
+        <KanbanContext.Provider value={{ kanbanData, setKanbanData }}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/task/:id" element={<TaskDetails />} />
+          </Routes>
+        </KanbanContext.Provider>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ kanban-board/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './header.module.scss';
+import UserMenu from '../UserMenu/UserMenu';
+
+const Header = () => {
+    return (
+      <header className={styles.header}>
+        <h1 className={styles.title}>Awesome Kanban Board</h1>
+        <UserMenu />
+      </header>
+    );
+  };
+  
+  export default Header;
