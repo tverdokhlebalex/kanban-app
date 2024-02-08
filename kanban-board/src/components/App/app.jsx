0f1f6889874dc594adf7b361ab48275ea7f91e61
Alexander <tverdokhlebalex@me.com>
2024-02-08 10:37:16 +0400

    column

--- kanban-board/src/components/App/app.jsx
+++ kanban-board/src/components/App/app.jsx
@@ -1,24 +1,35 @@
 import React, { useState, useEffect, createContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from '../Main/Main';
-import TaskDetails from '../TaskDetails/TaskDetails'; 
+import TaskDetails from '../TaskDetails/TaskDetails';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import styles from './App.module.scss'; 
 
-export const KanbanContext = createContext(null);
+export const KanbanContext = createContext({});
 
 const App = () => {
-  const [kanbanData, setKanbanData] = useState({});
+  const [kanbanData, setKanbanData] = useState({
+    Backlog: [],
+    Ready: [],
+    InProgress: [],
+    Finished: []
+  });
+  const [isDataInitialized, setIsDataInitialized] = useState(false);
 
   useEffect(() => {
-    const loadedData = JSON.parse(localStorage.getItem('kanbanData')) || {};
-    setKanbanData(loadedData);
+    const loadedData = JSON.parse(localStorage.getItem('kanbanData'));
+    if (loadedData) {
+      setKanbanData(loadedData);
+    }
+    setIsDataInitialized(true);
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('kanbanData', JSON.stringify(kanbanData));
-  }, [kanbanData]);
+    if (isDataInitialized) {
+      localStorage.setItem('kanbanData', JSON.stringify(kanbanData));
+    }
+  }, [kanbanData, isDataInitialized]);
 
   return (
     <Router>
--- kanban-board/src/components/Main/Main.jsx
+++ kanban-board/src/components/Main/Main.jsx
@@ -1,18 +1,19 @@
-import React, { useContext} from 'react';
-import { KanbanContext } from '../App/app';
-import styles from './Main.module.scss';
+import React, { useContext } from 'react';
+import { KanbanContext } from '../App/app'; 
 import Column from '../Column/Column';
+import styles from './Main.module.scss';
 
 const Main = () => {
   const { kanbanData } = useContext(KanbanContext);
 
   return (
     <main className={styles.main}>
-      {Object.entries(kanbanData).map(([column, tasks]) => (
-        <Column key={column} name={column} tasks={tasks} />
+      {Object.entries(kanbanData).map(([columnName, tasks]) => (
+        <Column key={columnName} name={columnName} tasks={tasks} />
       ))}
     </main>
   );
 };
 
 export default Main;
+
